216b6e37b67cfbf3bc5a6bba6efaea74d5d240db
KristinaPrro <kristina.kvc@yandex.ru>
2024-12-14 17:01:00 +0300

    3 create settings

---
+++ Assets/_Scripts/InstallerAndSettings/GameSettings.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameSettings", menuName = "GameSettings", order = 1)]
+public class GameSettings : ScriptableObject
+{
+	[field: Header("Enemy:")]
+	[field: SerializeField]
+	public int EnemyCountMin { get; private set; } = 3;
+	[field: SerializeField]
+	public int EnemyCountMax { get; private set; } = 10;
+	[field: SerializeField]
+	public float EnemyTimeOutMin { get; private set; } = 0.5f;
+	[field: SerializeField]
+	public float EnemyTimeOutMax { get; private set; } = 2f;
+	[field: SerializeField]
+	public float EnemySpeedMin { get; private set; } = 0.5f;
+	[field: SerializeField]
+	public float EnemySpeedMax { get; private set; } = 2f;
+	[field: SerializeField]
+	public int EnemyHealth { get; private set; } = 3;
+
+	[field: Header("Character:")]
+	[field: SerializeField]
+	public int CharacterHealth { get; private set; } = 3;
+	[field: SerializeField]
+	public float CharacterRadiusFire { get; private set; } = 3.5f;
+	[field: SerializeField]
+	public float CharacterRateFire { get; private set; } = 0.5f;
+	[field: SerializeField]
+	public int CharacterDamagePerShot { get; private set; } = 5;
+	[field: SerializeField]
+	public float CharacterBulletSpeed { get; private set; } = 2.5f;
+}
---
+++ Assets/_Scripts/InstallerAndSettings/GameUiSettings.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameUiSettings", menuName = "GameUiSettings", order = 1)]
+public class GameUiSettings : ScriptableObject
+{
+	[field: SerializeField]
+	public UiViewEnemyDamageElement PresenterEnemyDamageElement { get; private set; }
+
+	[field: Header("Screen:") ]
+	[field: SerializeField]
+	public UiViewStatusScreen PresenterStatusScreen { get; private set; }
+	[field: SerializeField]
+	public UiViewResultScreen PresenterResultScreen { get; private set; }
+}
