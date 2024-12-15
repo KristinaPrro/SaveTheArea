a530c6db0a0c12f9b2bee8fb389bcd237c26c941
KristinaPrro <kristina.kvc@yandex.ru>
2024-12-16 00:19:09 +0300

    3 upd IInputModel

--- Assets/_Scripts/Interface/IInputModel.cs
+++ Assets/_Scripts/Interface/IInputModel.cs
@@ -1,4 +1,8 @@
-﻿public interface IInputModel
-{
+﻿using System;
+using UnityEngine;
 
+public interface IInputModel
+{
+	IObservable<Vector2> DirectionMovementStream { get; }
+	Vector2 DirectionMovement { get; }
 }
--- Assets/_Scripts/Model/DesktopInputModel.cs
+++ Assets/_Scripts/Model/DesktopInputModel.cs
@@ -1,20 +1,46 @@
 ﻿using System;
+using UniRx;
+using UnityEngine;
 using Zenject;
 
-public class DesktopInputModel : IInputModel, IInitializable, IDisposable
+public class DesktopInputModel : IInputModel, ITickable
 {
+	private ReactiveProperty<Vector2> _directionMovementProperty = new();
+
+	public IObservable<Vector2> DirectionMovementStream => _directionMovementProperty;
+	public Vector2 DirectionMovement => _directionMovementProperty.Value;
 
 	public DesktopInputModel()
 	{
+		this.LogDebug($"");
 	}
 
-	public void Dispose()
+	public void Tick()
 	{
-
+		_directionMovementProperty.Value = GetDirection();
 	}
 
-	public void Initialize()
+	private Vector2 GetDirection()
 	{
+		var direction = Vector2.zero;
 
+		if (!Input.anyKey)
+			return direction;
+
+		if (Input.GetKey(KeyCode.W))
+			direction += Vector2.up;
+
+		if (Input.GetKey(KeyCode.S))
+			direction += Vector2.down;
+
+		if (Input.GetKey(KeyCode.D))
+			direction += Vector2.right;
+
+		if (Input.GetKey(KeyCode.A))
+			direction += Vector2.left;
+
+		this.LogDebug($"direction: {direction}");
+
+		return direction;
 	}
 }
