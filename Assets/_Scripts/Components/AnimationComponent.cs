6425664aa86297788622701a00b431d1acde8d7b
KristinaPrro <kristina.kvc@yandex.ru>
2024-12-16 16:07:27 +0300

    4 create AnimationComponent

---
+++ Assets/_Scripts/Components/AnimationComponent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class AnimationComponent : MonoBehaviour
+{
+	[field: SerializeField]
+	public Animator Animator { get; private set; }
+
+	public void Move(Vector2 direction)
+	{
+		this.LogDebug($"Move({direction})");
+	}
+
+	public void StopMoving()
+	{
+		this.LogDebug($"StopMoving()");
+	}
+
+	public void Fire()
+	{
+		this.LogDebug($"Fire()");
+	}
+
+	public void Hit()
+	{
+		this.LogDebug($"Hit()");
+	}
+
+	public void Die()
+	{
+		this.LogDebug($"Die()");
+	}
+}
