60af07f4aa5f19d2b3c6ae56d18b0ad3ce0e09f9
KristinaPrro <kristina.kvc@yandex.ru>
2024-12-16 16:01:04 +0300

    4 create view

---
+++ Assets/_Scripts/View/ViewAstronaut.cs
@@ -0,0 +1,22 @@
+﻿using ModestTree;
+using UnityEngine;
+
+public class ViewAstronaut : View
+{
+	[field: SerializeField]
+	public Rigidbody2D Rigidbody { get; private set; }
+	[field: SerializeField]
+	public Transform ContainerWeapon { get; private set; }
+	[field: SerializeField]
+	public Transform ContainerBullet { get; private set; }
+	[field: SerializeField]
+	public AnimationComponent AnimationComponent { get; private set; }
+
+	public void Awake()
+	{
+		Assert.IsNotNull(Rigidbody);
+		Assert.IsNotNull(ContainerWeapon);
+		Assert.IsNotNull(AnimationComponent);
+		Assert.IsNotNull(ContainerBullet);
+	}
+}
