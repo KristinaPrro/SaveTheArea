fb7ebdbf52223944996d1af8c7870f4540fba513
KristinaPrro <kristina.kvc@yandex.ru>
2024-12-17 21:25:13 +0300

    6 upd signals

--- Assets/_Scripts/Signals/SignalPlayerDamage.cs
+++ Assets/_Scripts/Signals/SignalPlayerDamage.cs
@@ -1,3 +1,11 @@
 public class SignalPlayerDamage
 {
+	public int DamageElementId { get; set; }
+	public int TargetId { get; set; }
+	
+	public SignalPlayerDamage(int damageElementId, int targetId)
+	{
+		DamageElementId = damageElementId;
+		TargetId = targetId;
+	}
 }
