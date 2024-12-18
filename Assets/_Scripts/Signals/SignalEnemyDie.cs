e66ff88b6e174ade9aa997c6b30a1bb640220f67
KristinaPrro <kristina.kvc@yandex.ru>
2024-12-18 19:47:00 +0300

    7 upd signals

---
+++ Assets/_Scripts/Signals/SignalEnemyDie.cs
@@ -0,0 +1,9 @@
+﻿public class SignalEnemyDie
+{
+	public int EnemyId { get; set; }
+	
+	public SignalEnemyDie(int enemyId)
+	{
+		EnemyId = enemyId;
+	}
+}
---
+++ Assets/_Scripts/Signals/SignalGameResults.cs
@@ -0,0 +1,11 @@
+﻿public class SignalGameResults
+{
+	public GameResultsData GameResultsData { get; private set; }
+	public bool IsWin { get; private set; }
+
+	public SignalGameResults(bool isWin, GameResultsData gameResultsData)
+	{
+		GameResultsData = gameResultsData;
+		IsWin = isWin;
+	}
+}
