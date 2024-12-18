7b72f9ad8b3db11bc13d67af9ed7651bc1ff9190
KristinaPrro <kristina.kvc@yandex.ru>
2024-12-18 19:55:47 +0300

    7 create GameResultsData

---
+++ Assets/_Scripts/Data/GameResultsData.cs
@@ -0,0 +1,15 @@
+﻿public struct GameResultsData
+{
+	public int CurrentPlayerHealth { get; private set; }
+	public int MaxPlayerHealth { get; private set; }
+	public int CurrentEnemyCount { get; private set; }
+	public int MaxEnemyCount { get; private set; }
+
+	public GameResultsData(int currentPlayerHealth, int maxPlayerHealth, int currentEnemyCount, int maxEnemyCount)
+	{
+		CurrentPlayerHealth = currentPlayerHealth;
+		MaxPlayerHealth = maxPlayerHealth;
+		CurrentEnemyCount = currentEnemyCount;
+		MaxEnemyCount = maxEnemyCount;
+	}
+}
