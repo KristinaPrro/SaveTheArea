78ad3c5ff8f6d6d313d3e9021274e3757517751a
KristinaPrro <kristina.kvc@yandex.ru>
2024-12-24 01:39:28 +0300

    11 create LogChannel

--- Assets/_Scripts/Extensions/Logs.cs
+++ Assets/_Scripts/Extensions/Logs.cs
@@ -1,22 +1,39 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public static class Logs
 {
-	private static bool IsDebagLogs = true;
+	private static bool IsDebagMode = true;
+	private readonly static Dictionary<LogChannel, (bool isActive, string color)> _types = new()
+	{
+			[LogChannel.Debug] = (true, "ff00ffff"),
+			[LogChannel.Default] = (true, "ffffffff"),
+			[LogChannel.SpawnObject] = (true, "a0ffddff"),
+			[LogChannel.Moving] = (true, "ffeaa0ff"),
+			[LogChannel.Animation] = (true, "b0b0b0ff"),
+	};
 
 	public static void LogDebug(this object obj,
 		string message = "",
+		LogChannel logType = LogChannel.Debug,
 		[CallerMemberName] string callerMethodName = "")
 	{
-		if (!IsDebagLogs)
+		if (!IsDebagMode)
 			return;
 
-		Debug.LogWarningFormat(GetMessage(obj, callerMethodName, $"<color=#ff00ffff>/// {message}</color>"));
+		var channelInfo = CheckChannelInfo(logType);
+
+		if (!channelInfo.isActive)
+			return;
+
+		Debug.LogWarningFormat(GetMessage(obj, callerMethodName, $"<color=#{channelInfo.color}>/// {message}</color>")
+			.AddActualTimeLogs());
 	}
 
 	public static void Log(this object obj,
 		string message = "",
+		LogChannel logType = LogChannel.Default,
 		[CallerMemberName] string callerMethodName = "")
 	{
 		Debug.LogFormat(GetMessage(obj, callerMethodName, message));
@@ -24,6 +41,7 @@ public static class Logs
 
 	public static void LogWarning(this object obj,
 		string message = "",
+		LogChannel logType = LogChannel.Default,
 		[CallerMemberName] string callerMethodName = "")
 	{
 		Debug.LogWarningFormat(GetMessage(obj, callerMethodName, message));
@@ -36,8 +54,24 @@ public static class Logs
 		Debug.LogErrorFormat(GetMessage(obj, callerMethodName, message));
 	}
 
+	private static (bool isActive, string color) CheckChannelInfo(LogChannel logChannel)
+	{
+		if(!_types.TryGetValue(logChannel, out var info))
+		{
+			Debug.LogErrorFormat($"Not found '{logChannel}'!");
+			return (false, "#ff0000ff") ;
+		}
+
+		return info;
+	}
+
 	private static string GetMessage(object obj, string callerMethodName, string message)
 	{
 		return $"[{obj?.GetType()}] {callerMethodName}() {message}";
+	}
+
+	private static string AddActualTimeLogs(this string log)
+	{
+		return $"{log} \n realtimeSinceStartup: {Time.realtimeSinceStartup} frame: {Time.frameCount}";
 	}
 }
