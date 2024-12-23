7ded9b231fbf42377ea48a56a65b6257ea62ec2e
KristinaPrro <kristina.kvc@yandex.ru>
2024-12-23 23:27:27 +0300

    10 create data for DamageElement

---
+++ Assets/_Scripts/Data/DamageElementData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public struct DamageElementData
+{
+	public int Id { get; private set; }
+	public int Damage { get; private set; }
+	public float Speed { get; private set; }
+	public Transform StartPosition { get; private set; }
+
+	public DamageElementData(int id, int damage, float speed, Transform startPosition)
+	{
+		Id = id;
+		Damage = damage;
+		Speed = speed;
+		StartPosition = startPosition;
+	}
+}
---
+++ Assets/_Scripts/Data/TargetData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public struct TargetData
+{
+	public Transform Transform { get; private set; }
+	public float Speed { get; private set; }
+	public Vector2 DirectionMovement { get; private set; }
+
+	public TargetData(Transform transform, float speed, Vector2 directionMovement)
+	{
+		Transform = transform;
+		Speed = speed;
+		DirectionMovement = directionMovement;
+	}
+}
