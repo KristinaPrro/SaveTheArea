e200c273ceee31c94607ed263d094e50c2514912
KristinaPrro <kristina.kvc@yandex.ru>
2024-12-23 20:27:37 +0300

    10 create enemyData

---
+++ Assets/_Scripts/Data/EnemyData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public struct EnemyData
+{
+	public int Id { get; private set; }
+	public Transform StartPosition { get; private set; }
+	public int Health { get; private set; }
+	public float Speed { get; private set; }
+
+	public EnemyData(int id, Transform startPosition, int health, float speed)
+	{
+		Id = id;
+		StartPosition = startPosition;
+		Health = health;
+		Speed = speed;
+	}
+}
