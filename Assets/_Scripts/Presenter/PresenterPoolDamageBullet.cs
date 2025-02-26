fbddb8af8c356173698a722a3a9d9bcb6b2cccc7
KristinaPrro <kristina.kvc@yandex.ru>
2025-02-26 18:16:30 +0300

    12 temporary edit for target

--- Assets/_Scripts/Presenter/PresenterPoolDamageBullet.cs
+++ Assets/_Scripts/Presenter/PresenterPoolDamageBullet.cs
@@ -14,12 +14,15 @@ public class PresenterPoolDamageBullet : PresenterPoolDamageElement<ViewPoolDama
 	public void SetTarget(TargetData targetData)
 	{
 		Vector2 startTargetPosition = targetData.Transform.position;
-		var startPosition = View.transform.position;
+		Vector2 startPosition = View.transform.position;
 		var acceptableError = targetData.Transform.lossyScale.y / 2;
-		var sumSpeed = Speed + targetData.Speed;
+		var sumSpeed = Speed + targetData.Speed;//!?
 
 		var shiftTargetPosition = startTargetPosition;
 		var meetPosition = startTargetPosition;
+
+		this.LogDebug($"! start {startTargetPosition}, {startPosition}, {acceptableError}, {sumSpeed}, {shiftTargetPosition}, {meetPosition}");
+		var i = 0; //todo
 
 		do
 		{
@@ -29,19 +32,22 @@ public class PresenterPoolDamageBullet : PresenterPoolDamageElement<ViewPoolDama
 
 			var time = distance / sumSpeed;
 			shiftTargetPosition = startTargetPosition + targetData.DirectionMovement * targetData.Speed * time;
-		}
-		while (Vector2.Distance(meetPosition, shiftTargetPosition) < acceptableError);
 
-		DirectionMovement = Vector3.ClampMagnitude(shiftTargetPosition - (Vector2)startPosition, 1);
+			this.LogDebug($"! do {meetPosition}, {shiftTargetPosition}, {Vector2.Distance(meetPosition, shiftTargetPosition)} < {acceptableError};     " +
+				$"{distance}({Vector2.Distance(meetPosition, startTargetPosition)}+{Vector2.Distance(meetPosition, startPosition)}), {time}, ");
+		}
+		while (Vector2.Distance(meetPosition, shiftTargetPosition) > acceptableError && i++<5);
+
+		DirectionMovement = Vector3.ClampMagnitude(shiftTargetPosition - startPosition, 1);
+		this.LogDebug($"! finish {DirectionMovement}");
 	}
 
 	public class Factory : PlaceholderFactory<Transform, DamageElementData, PresenterPoolDamageBullet>
 	{
 		public PresenterPoolDamageBullet Create(Transform param, DamageElementData data, TargetData targetData)
 		{
-			 var presenter = base.Create(param, data);
+			var presenter = base.Create(param, data);
 			presenter.SetTarget(targetData);
-
 			return presenter;
 		}
 	}
